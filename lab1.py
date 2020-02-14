fdd558e7810bb72db9d65fd72847561674d0f06f
chatchard47 <46014483+chatchard47@users.noreply.github.com>
2020-02-14 22:03:05 +0700

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,13 +1,13 @@
 # show message
 print("Hellow world") 
-print("chatchard choothai\n"); #แสดงข้อความขึ้นบรรทัดใหม่    
+print("chatchard choothai\n") #แสดงข้อความขึ้นบรรทัดใหม่    
 print("i love you\n"*3) #แสดงข้อความทำซ้ำ *3 บรรทัดใหม่  
 
 #เก็บค่าลงในตัวแปร์ 1 
 str = "Basic Python programing."
 
 print (str)
-print (str,'My name is '); # การเชื่อมข้อความด้วยเครื่องหมาย , 
+print (str,'My name is ') # การเชื่อมข้อความด้วยเครื่องหมาย , 
 print (str[0])   #แสดงการเข้าถึงช่วงใน string ตัวแรก
 print (str[-1])   #แสดงการเข้าถึงช่วงใน string ตัวสุดท้าย
 print (str[0:5]) #แสดงการเข้าถึงช่วงใน string ตัว 0-5
@@ -17,10 +17,10 @@ print ("Python"in str) #เช็กข้อความมีใน str หร
 print ("555"not in str) #เช็กข้อความไม่มีใน str หรือไม่
 
 #เก็บค่าลงในตัวแปร์ 2 
-str1 = "March";
-str2 = "Chatchard นะจ๊ะ";
+str1 = "March"
+str2 = "Chatchard นะจ๊ะ"
 
-print (str1+" @ "+str2); #การเชื่อมข้อความด้วยเครื่องหมาย + 
+print (str1+" @ "+str2) #การเชื่อมข้อความด้วยเครื่องหมาย + 
 
 #แสดงทศนิยม
 print ("%.4f"%50)
